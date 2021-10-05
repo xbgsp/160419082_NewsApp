e38ce73300bc416a24758e00953d750fa3e3b95b
xbgsp <bagusp248@gmail.com>
2021-10-05 19:49:02 +0700

    faq done! mail masih belum pasti dibuat/tidak

--- app/src/main/java/com/ubaya/a160419082_newsapp/view/FaqFragment.kt
+++ app/src/main/java/com/ubaya/a160419082_newsapp/view/FaqFragment.kt
@@ -6,6 +6,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.ubaya.a160419082_newsapp.R
+import com.ubaya.a160419082_newsapp.util.loadImage
+import kotlinx.android.synthetic.main.fragment_faq.view.*
 
 class FaqFragment : Fragment() {
     override fun onCreateView(
@@ -14,5 +16,11 @@ class FaqFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_faq, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.imageViewFaq.loadImage("https://adveror.com/ikootchi/2020/09/faq-alfa-forni.png", view.progressBarFaq)
     }
 }
