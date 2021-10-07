3d3f79400a5722ebb366d606da25bb36472b4d20
xbgsp <bagusp248@gmail.com>
2021-10-07 18:04:29 +0700

    status ok!

--- app/src/main/java/com/ubaya/a160419082_newsapp/view/AddNewsFragment.kt
+++ app/src/main/java/com/ubaya/a160419082_newsapp/view/AddNewsFragment.kt
@@ -1,4 +1,4 @@
-package com.ubaya.a160419082_newsapp
+package com.ubaya.a160419082_newsapp.view
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -7,7 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.navigation.Navigation
-import com.ubaya.a160419082_newsapp.view.ProfileFragmentDirections
+import com.ubaya.a160419082_newsapp.R
 import kotlinx.android.synthetic.main.fragment_add_news.*
 
 class AddNewsFragment : Fragment() {
@@ -25,6 +25,7 @@ class AddNewsFragment : Fragment() {
         btnAddNews.setOnClickListener {
             val toast = Toast.makeText(context, "News added succesfully!", Toast.LENGTH_SHORT)
             toast.show()
+
             val action = AddNewsFragmentDirections.actionMyNews()
             Navigation.findNavController(it).navigate(action)
         }
